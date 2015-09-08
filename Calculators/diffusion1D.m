4bfdbbe0f141367ab2c279c7a2f13635fdce1d05
lauflulu <lauflulu@users.noreply.github.com>
2015-09-08 19:03:40 +0200

    next

--- Calculators/diffusion1D.m
+++ Calculators/diffusion1D.m
@@ -1,5 +1,5 @@
 % Simulating the 1-D Diffusion equation (Fourier's equation) by the
-...Finite Difference Method(a time march) change
+...Finite Difference Method(a time march)
 % Numerical scheme used is a first order upwind in time and a second order
 ...central difference in space (both Implicit and Explicit)
 

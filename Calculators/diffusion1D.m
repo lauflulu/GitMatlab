a7b6c608aa14a238a090528e5beaf724d5a0df3c
blackpenny <matthaeus@mytum.de>
2015-09-08 18:09:51 +0200

    1st version

--- Calculators/diffusion1D.m
+++ Calculators/diffusion1D.m
@@ -8,19 +8,19 @@
 nx=100;              %Number of steps in space(x)
 nt=200;              %Number of time steps 
 dt=1;                %Width of each time step
-dx=2/(nx-1);         %Width of space step
-x=0:dx:2;            %Range of x (0,2) and specifying the grid points
+dx=1/(nx-1);         %Width of space step
+x=0:dx:1;            %Range of x (0,2) and specifying the grid points
 u=zeros(nx,1);       %Preallocating u
 un=zeros(nx,1);      %Preallocating un
-vis=1;            %Diffusion coefficient/viscosity
-beta=vis*dt/(dx*dx); %Stability criterion (0<=beta<=0.5, for explicit)
+Dv=1;                %Diffusion coefficient/viscosity
+beta=Dv*dt/(dx*dx);  %Stability criterion (0<=beta<=0.5, for explicit)
 UL=1;                %Left Dirichlet B.C
 UR=0;                %Right Dirichlet B.C
 UnL=1;               %Left Neumann B.C (du/dn=UnL) 
 UnR=1;               %Right Neumann B.C (du/dn=UnR) 
 
 %%
-%Initial Conditions: A square wave
+%Initial Conditions: A square wave -> is set to zero for all x
 for i=1:nx
     if ((0<=x(i))&&(x(i)<=0.0))
         u(i)=0;
@@ -30,13 +30,13 @@ for i=1:nx
 end
 
 %%
-%B.C vector
-bc=zeros(nx-2,1);
-bc(1)=vis*dt*UL/dx^2; bc(nx-2)=vis*dt*UR/dx^2;  %Dirichlet B.Cs
-%bc(1)=-UnL*vis*dt/dx; bc(nx-2)=UnR*vis*dt/dx;  %Neumann B.Cs
-%Calculating the coefficient matrix for the implicit scheme
-E=sparse(2:nx-2,1:nx-3,1,nx-2,nx-2);
-A=E+E'-2*speye(nx-2);        %Dirichlet B.Cs
+%B.C vector 
+bc=zeros(nx-2,1); % concentration at boundary
+bc(1)=Dv*dt*UL/dx^2; bc(nx-2)=Dv*dt*UR/dx^2;  %Dirichlet B.Cs
+%bc(1)=-UnL*Dv*dt/dx; bc(nx-2)=UnR*Dv*dt/dx;  %Neumann B.Cs
+%Calculating the coefficient matrix for the implicit scheme (?)
+E=sparse(2:nx-2,1:nx-3,1,nx-2,nx-2); % sparse - big matrix efficient calculator
+A=E+E'-2*speye(nx-2);        %Dirichlet B.Cs speye - speye(n) is a n x n identity matrix created with sparse
 %A(1,1)=-1; A(nx-2,nx-2)=-1; %Neumann B.Cs
 D=speye(nx-2)-(vis*dt/dx^2)*A;
 

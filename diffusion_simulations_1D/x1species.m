98dc4a8fb75978ff33b816ec34cdd24ada5c0562
lauflulu <lauflulu@users.noreply.github.com>
2015-09-14 19:02:55 +0200

    Changed to Crank-Nicholson scheme boundary conditions have to be checked

--- diffusion_simulations_1D/x1species.m
+++ diffusion_simulations_1D/x1species.m
@@ -14,6 +14,7 @@ nt=time/dt;       %Number of time steps
 dx=width/(nx-1);         %Width of space step (200 µm)
 x=0:dx:width;            %Range of x (0,2) and specifying the grid points
 dtmax=(20*20)/(D*4*pi*pi);  %Stability criterion dt<(lmin^2/(Dv*4*pi*pi), for implicit)
+beta=D*dt/dx^2; %dimensionless diffusion parameter
 
 %% if nothing is done here, the initial conditions will be u=0 everywhere (but at boundary)
 u=zeros(nx,1);       %initialize concentration profile u
@@ -21,16 +22,16 @@ un=zeros(nx,1);      %initialize un
 u=u+icAddDroplet(200,20,x,nx); %2x 40 µm droplets projected to 1D
 %u=u+icAddSquare(1.0,0.2,x,nx); %a square wave
 
-
 %% boundary conditions
 bc=bcDirichlet(D,dt,dx,nx,0,0); %reservoir bcs
 %bc=bcNeumann(D,dt,dx,nx,0,0); %(zero-)flux bcs
 
-%% coefficient matrix for the implicit scheme
+%% coefficient matrix for the Crank-Nicholson scheme A*U_n+1=B*U_n
 E=sparse(2:nx-2,1:nx-3,1,nx-2,nx-2); % 1st off-diagonal of 1s
-A=E+E'-2*speye(nx-2);        %Dirichlet: matrix with 1s at off-diagonals and -2s at diagonal
+A=-beta*E-beta*E'+2*(1+beta)*speye(nx-2);  %Dirichlet (only U is modified): matrix with 1s at off-diagonals and -2s at diagonal
 %A(1,1)=-1; A(nx-2,nx-2)=-1; %Neumann:
-C=speye(nx-2)-(D*dt/dx^2)*A;
+B=beta*E+beta*E'+2*(1-beta)*speye(nx-2);
+
 
 %% Calculating the concentration profile for each time step
 i=2:nx-1;
@@ -52,13 +53,7 @@ for it=0:nt
     
     U=un;U(1)=[];U(end)=[];
     U=U+bc;
-    U=C\U;
+    U=B/A*U;
     u=[0;U;0];                      %Dirichlet [UL;U;UR]; 
     %u=[U(1)-UnL*dx;U;U(end)+UnR*dx]; %Neumann
-    %}
-    %-------------------
-    %Explicit method with F.D in time and C.D in space
-    %{
-    u(i)=un(i)+(Dv*dt*(un(i+1)-2*un(i)+un(i-1))/(dx*dx));
-    %}
 end
